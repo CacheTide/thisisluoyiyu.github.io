f29a87631807109aaa14e49a47e69577757dd3d0
cxzlw <cxz.lwnb@gmail.com>
2026-04-14 21:27:23 +0800

    我不造啊

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+// import RemoteAssets from 'vite-plugin-remote-assets'
+
+export default defineConfig({
+  // todo wait upyun assets
+  // plugins: [RemoteAssets()],
+
+  optimizeDeps: {
+    include: ['vue-gtag-next'],
+  },
+})
