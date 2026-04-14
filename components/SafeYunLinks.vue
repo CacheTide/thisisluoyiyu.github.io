b816a4ca4cd58ec7b1c0d05150a4079634e168fb
thisisluoyiyu <thisisyiyu@gmail.com>
2026-04-14 18:24:56 +0800

    fix:frindelinks加载bug

--- components/SafeYunLinks.vue
+++ components/SafeYunLinks.vue
@@ -1,22 +1,37 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 
 const props = defineProps({
-  links: Array,
+  links: {
+    type: Array,
+    default: () => []
+  },
   random: Boolean
 })
 
 const data = ref([])
 
 onMounted(() => {
-  if (props.random) {
-    data.value = [...props.links].sort(() => Math.random() - 0.5)
-  } else {
-    data.value = [...props.links]
+  if (props.links && props.links.length) {
+    if (props.random) {
+      data.value = [...props.links].sort(() => Math.random() - 0.5)
+    } else {
+      data.value = [...props.links]
+    }
+  }
+})
+
+watch(() => props.links, (newVal) => {
+  if (newVal && newVal.length && data.value.length === 0) {
+    if (props.random) {
+      data.value = [...newVal].sort(() => Math.random() - 0.5)
+    } else {
+      data.value = [...newVal]
+    }
   }
 })
 </script>
 
 <template>
-  <YunLinks :links="data.length ? data : links" :random="false" />
+  <YunLinks :links="data.length ? data : props.links" :random="false" />
 </template>
